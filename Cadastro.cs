273b058ef46f8065f966cc8de26a34ae250258ec
ronaldo <ronaldo.dsantos@gmail.com>
2024-10-03 22:50:23 -0300

    Classe Selada, Classe Abstrata e Record

--- Cadastro.cs
+++ Cadastro.cs
@@ -16,6 +16,15 @@ namespace Cadastro
 
     public string Descricao { get; set; } // Propriedade | Public torna a propriedade pública e com isso é possível utilizá-la em outras classes
 
+    // Propriedade somente leitura são propriedades que não permitem a modificação de seus valores, a não ser que seja modificada pelo construtor da classe
+    public int Estoque { get; } // Maneiras de declarar uma propriedade como somente leitura
+    // public readonly int Estoque; // Maneiras de declarar a propriedade como somente leitura
+
+    // Contrutor é uma ação que é executada no momento que a classe está sento instanciada e através desse construtor conseguimos ter acesso aos atributos e métodos da classe e de outras classes também
+    public Produto()
+    {
+      Estoque = 2; // Setando valor a propriedade somente leitura estoque, por ser uma propriedade privada só é possível setar valor no momento de construção da classe, isso cria uma proteção de acesso a essa propriedade, pois quando for instanciada por outros objetos essa propriedade não poderá ser alterada
+    }
     public void ImprimirDescricao() // Método
     {
       Console.WriteLine(GetId() + " - " + Descricao);
@@ -31,4 +40,115 @@ namespace Cadastro
       return Id;
     }
   }
+
+  // Super classe que será herdada por outras sub classes
+  public class Pessoa // Super Classe Pessoa
+  {
+    public int Id { get; set; }
+
+    public string Endereco { get; set; }
+
+    public string Cidade { get; set; }
+
+    public string Cep { get; set; }
+
+    public void ImprimirDados()
+    {
+      Console.WriteLine("Código: " + Id);
+      Console.WriteLine("Endereço: " + Endereco);
+      Console.WriteLine("Cidade: " + Cidade);
+      Console.WriteLine("Cep: " + Cep);
+    }
+  }
+
+  // Herança
+  public class PessoaFisica : Pessoa // Sub classe PessoaFisica herdando os atributos e métodos da classe pessoa
+  {
+    public string CPF { get; set; }
+
+    public void ImprimirCPF() // Método exclusivo da sub classe
+    {
+      Console.WriteLine("CPF: " + CPF);
+    }
+  }
+
+  // Herança
+  public class Funcionario : PessoaFisica // Sub classe Funcionario herdando os atributos e métodos da classe PessoaFisica e consequentemente da classe Pessoa
+  {
+    public string Matricula { get; set; }
+
+    public void ImprimirMatricula()
+    {
+      Console.WriteLine("Matrícula: " + Matricula);
+    }
+  }
+
+  // Classe Selada (É uma classe como as demais, porém não pode ser herdada por outras classes, essa é a principal utilização da classe selada, forçar que ela não possa ser herdada por nenhuma outra classe, mas pode ser instanciada e utilizada por outras classes normalmente)
+  public sealed class Configuracao // sealed = Classe Selada
+  {
+    public string Host { get; set; }
+  }
+
+  // Classe Abstrata (É uma classe que não pode ser instanciada, isso faz que a classe abstrata tenha o propósito de ser uma super classe para ser instanciadas por outras sub classes, uma espécie de contrato para forçar que as sub classes implemente de forma concreta o que é informado na classe abstrata)
+  public abstract class Animal // abstract = Classe Abstrata
+  {
+    public string Nome { get; set; }
+
+    public abstract string GetInformacoes(); // abstract para forçar que a sub classe que estiver herdando a classe animal implemente também este método (Obs. método sem instruções que precisarão ser informadas pela classe que está herdando) 
+
+    public void ImprimirDados()
+    {
+      Console.WriteLine("Nome animal: " + Nome);
+      Console.WriteLine("Informações: " + GetInformacoes());
+    }
+  }
+  // sub classe Cachorro herdando a classe abstrata Animal
+  public class Cachorro : Animal
+  {
+    public override string GetInformacoes() // Complementando o método GetInformacoes que estava sem instruções
+    {
+      return "Cachorro é um bom amigo";
+    }
+  }
+
+  // Record
+  public class Curso // Classe de exemplo para mostrar como teria que ser feito a comparação de valores das instâncias sem o Record, neste exemplo se substituir a palava class para record, os métodos criados para comparação não se fazem necessários
+  {
+    public int Id { get; set; }
+
+    public string Descricao { get; set; }
+
+    // Método para alterar o comportamento do Equals para verificar se os valores das instancias são iguais, por padrão o retorno seria false porque ele analisa se as instancias são iguais
+    public override bool Equals(object? obj)
+    {
+      if (obj == null) return false;
+
+      if (obj is Curso curso)
+      {
+        return Id == curso.Id && Descricao == curso.Descricao;
+      }
+
+      return base.Equals(obj);
+    }
+
+    // Método para alterar o comportamento do == para verificar se os valores das instancias são iguais, por padrão o retorno seria false porque ele analisa se as instancias são iguais
+    public static bool operator ==(Curso a, Curso b)
+    {
+      return a.Equals(b);
+    }
+
+    public static bool operator !=(Curso a, Curso b)
+    {
+      return !(a == b);
+    }
+  }
+
+  public record CursoRecord(int Id, string Descricao); // Criando um record com duas propriedades somente leitura
+
+  public class CursoTeste
+  {
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+  }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿AulaClasses();
+﻿//AulaClasses();
+//AulaPropriedadeSomenteLeitura();
+//AulaHeranca();
+//AulaClasseSelada();
+//AulaClasseAbstrata();
+AulaRecord();
 
 void AulaClasses()
 {
@@ -8,9 +13,97 @@ void AulaClasses()
 
   // Maneira de utilizar uma classe não estática
   var produto = new Cadastro.Produto(); // Instanciando a classe produto para utilização, por ser classe não estática que precisa ser instanciada
-  
+
   produto.Descricao = "Teclado"; // Informando um valor para o atributo descrição
   produto.SetId(1); // Chamando o método SetId e passando um valor por parâmetro
   produto.ImprimirDescricao(); // Chamando o método ImprimirDescricao
   Console.WriteLine(produto.GetId());
+}
+
+void AulaPropriedadeSomenteLeitura()
+{
+  var produto = new Cadastro.Produto();
+
+  produto.Descricao = "Mouse";
+  // produto.Estoque = 1; Não conseguimos atribuir valor dessa maneira porque se trata de uma propriedade somente leitura
+  Console.WriteLine(produto.Estoque + " - " + produto.Descricao); // Exibindo o valor da propriedade somente leitura estoque
+}
+
+void AulaHeranca()
+{
+  // Utilizando a classe pessoa física
+  var pessoaFisica = new Cadastro.PessoaFisica();
+
+  pessoaFisica.Id = 1;
+  pessoaFisica.Endereco = "Endereço Teste";
+  pessoaFisica.Cidade = "Cidade Teste";
+  pessoaFisica.Cep = "12345-678";
+  pessoaFisica.CPF = "123.456.789-10";
+
+  pessoaFisica.ImprimirDados();
+  pessoaFisica.ImprimirCPF();
+
+  // Utilizando a classe funcionário
+  var funcionario = new Cadastro.Funcionario();
+
+  funcionario.Id = 2;
+  funcionario.Endereco = "Endereço Teste 2";
+  funcionario.Cidade = "Cidade Teste 2";
+  funcionario.Cep = "12345-678";
+  funcionario.CPF = "123.456.789-10";
+  funcionario.Matricula = "12345";
+
+  funcionario.ImprimirDados();
+  funcionario.ImprimirCPF();
+  funcionario.ImprimirMatricula();
+}
+
+void AulaClasseSelada()
+{
+  /*
+  var configuracao = new Cadastro.Configuracao();
+  configuracao.Host = "localhost";
+  */
+
+  var configuracao = new Cadastro.Configuracao // Outra maneira de instanciar a classe já atribuindo valor as propriedades
+  {
+    Host = "localhost"
+  };
+
+  Console.WriteLine(configuracao.Host);
+}
+
+void AulaClasseAbstrata()
+{
+  var cachorro = new Cadastro.Cachorro();
+
+  cachorro.Nome = "Dog";
+  cachorro.ImprimirDados();
+}
+
+void AulaRecord()
+{
+  // Exemplo de comparando valores sem e com o Record (solução com o retord no namespace cadastro)
+  var curso01 = new Cadastro.Curso { Id = 1, Descricao = "Curso" };
+  var curso02 = new Cadastro.Curso { Id = 1, Descricao = "Curso" };
+  Console.WriteLine(curso01 == curso02); // Comparando se os valores das instancias são iguais
+  Console.WriteLine(curso01.Equals(curso02)); // Comparando se os valores das instancias são iguais
+
+  // Exemplo de como copiar dados de uma instancia para outra sem o Record
+  var cursoTeste01 = new Cadastro.CursoTeste { Id = 1, Descricao = "Curso" };
+  //var cursoTeste02 = cursoTeste01;
+  //cursoTeste02.Descricao = "TESTE TESTE"; // Desta forma um valor vai sobrepor o outro
+  var cursoTeste02 = new Cadastro.CursoTeste();
+  cursoTeste02.Descricao = "TESTE TESTE";
+  Console.WriteLine(cursoTeste01.Descricao);
+  Console.WriteLine(cursoTeste02.Descricao);
+
+  // Record
+  var curso03 = new Cadastro.CursoRecord(1, "Curso"); // Instanciando e atribuindo valores com record
+  Console.WriteLine(curso03.Id + " - " + curso03.Descricao);
+
+  // Exemplo de como copiar dados de uma instancia para outra com o Record
+  var cursoTeste03 = curso03 with { Descricao = "Teste Record" }; // Copiando os valores da instancia curso 03 alterando apenas o atributo descrição
+  Console.WriteLine(curso03.Descricao);
+  Console.WriteLine(cursoTeste03.Descricao);
 }
